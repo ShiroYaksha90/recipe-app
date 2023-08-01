9d308ee943914366c2c351e222f03a81587e1b21
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-01 13:54:16 +0300

    Create models & add association

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,5 @@
 class Food < ApplicationRecord
+    belongs_to :user
+    has_many :food_recipe
     validates :name, :measurement_unit, :price, :quantity, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+    has_many :recipe
+    has_many :food
     validates :name, presence: true
 end

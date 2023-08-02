a7b2136329c3655f3103f003b4753e99075ea031
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-03 00:36:05 +0300

    Fix linter issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :authenticate_user!
-  
-    protected
-  
-    def configure_permitted_parameters
-        attributes = [:name]
-        devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
-        devise_parameter_sanitizer.permit(:account_update, keys: attributes)
-      end
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action  :authenticate_user!, only: %i[show edit update destroy]
+  before_action :authenticate_user!, only: %i[show edit update destroy]
   before_action :set_recipe, only: %i[show update]
 
   def index

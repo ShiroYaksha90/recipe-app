c2f490d85c0faed077205a10ce1fb47bc712ee70
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-02 17:26:57 +0300

    Update user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,23 @@ class UsersController < ApplicationController
   # before_action :authenticate_user
 
   def index
-    @user = User.all
+    @users = User.all
+  end
+
+  def show
+    set_user
+  end
+
+  private
+
+  def set_user
+    if params[:id] == 'sign_out'
+      sign_out current_user if user_signed_in?
+      redirect_to new_user_session_path
+    elsif current_user&.id == params[:id].to_i
+      @user = current_user
+    else
+      @user = User.find(params[:id])
+    end
   end
 end

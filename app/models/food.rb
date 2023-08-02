9fd1afce02da19e7b50c08d7735fbd752ca2064b
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-02 17:46:53 +0300

    Fix linter issues

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Food < ApplicationRecord
   belongs_to :user
   has_many :food_recipe
--- app/models/food_recipe.rb
+++ app/models/food_recipe.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class FoodRecipe < ApplicationRecord
   belongs_to :recipe
   belongs_to :food

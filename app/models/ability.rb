1eb221add61a80c7eb59a05ce8487f598006734a
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-04 14:36:16 +0300

    Add more configurtion

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,13 @@ class Ability
 
   def initialize(user)
     # Define abilities for the user here. For example:
+    can :read, :all
+    cannot :read, Recipe, public:false
     return unless user.present?
 
     can(:manage, Recipe, user:)
-
+    can(:manage, Food, user:)
+    can(:manage, RecipeFood, user:)
     return unless user.role == 'admin'
 
     can :manage, all
